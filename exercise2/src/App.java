88e432f37c386ed63176430feac78a73ac2638ec
Nelson <Nelsonnavarro3005@gmail.com>
2026-09-09 13:08:53 -0500

    Update2

--- exercise2/src/App.java
+++ exercise2/src/App.java
@@ -1,9 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Figura rectangulo = new Rectangulo(5, 10, "Rectángulo");
-        Figura cuadrado = new Cuadrado(4, "Cuadrado");
-        Figura circulo = new Circulo(3, "Círculo");
-        Figura triangulo = new Triangulo(6, 8, "Triángulo");
+        Figura rectangulo = new Rectangulo(3, 6, "Rectángulo");
+        Figura cuadrado = new Cuadrado(5, "Cuadrado");
+        Figura circulo = new Circulo(4, "Círculo");
+        Figura triangulo = new Triangulo(3, 4, "Triángulo");
 
         System.out.println("Área del " + rectangulo.getTipo() + ": " + rectangulo.area());
         System.out.println("Área del " + cuadrado.getTipo() + ": " + cuadrado.area());

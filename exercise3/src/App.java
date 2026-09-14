c797acc5c70762977698651ab32e7163a48c3cca
Pablinho <gonzalez[juan18@gmail.com>
2026-09-14 08:13:12 -0500

    Organiza exercise3 correctamente

---
+++ exercise3/src/App.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Lector l1 = new Lector("Jesus","1002026087" );
+        Lector l2 = new Lector("Luis", "2020090912");
+
+        Libro b1 = new Libro("Dante Alligheri", "La Divina Comedia", true);
+        Libro b2 = new Libro("Gabriel Garcia M.", "Cien anos de soledad", disponible: true);
+        Libro b3 = new Libro("Edgar Alan P.", "El gato Negro", true)
+        
+    }
+}
